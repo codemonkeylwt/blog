c653aa420c09efa528996c403e9610430b6f0c0d
刘文韬 <liuwentao8713@adpanshi.com>
2019-04-03 19:31:03 +0800

    修复关闭原有服务脚本

--- shell/stop_all.sh
+++ shell/stop_all.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-pids = jps |  grep 'blog' | awk '{print $1}'
+pids=$(jps |  grep 'blog' | awk '{print $1}')
 
-if [[ pids ]];then
+if [[ $pids ]];then
         jps |  grep 'blog' | awk '{print $1}' | xargs kill -15
     else
         echo "service is not running"
